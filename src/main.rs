95495aa8d1f8d5c083789e6a75c8a3d2f286f26f
Angelo <angelosalgueiro20@gmail.com>
2026-06-30 21:21:40 +0200

    adding calculator number and input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use slint::SharedString;
+
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
+fn main(){
+    let main_window = MainWindow::new().unwrap();
+    let weak_window = main_window.as_weak();
 
-    main_window.run()
+    main_window.on_add_number(move |value: SharedString| {
+        let ui = weak_window.unwrap();
+        let text = ui.get_input_value().to_string();
+        let new_value = SharedString::from(format!("{}{}", text, value));
+        ui.set_input_value(new_value);
+    });
+
+    main_window.run().unwrap();
 }
